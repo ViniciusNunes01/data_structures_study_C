a63d35e0c91293a7832fd228c83a25711e494a85
ViniciusNunes01 <sanjip415@gmail.com>
2026-03-17 11:41:28 -0300

    struct

--- studies/aula3.c
+++ studies/aula3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // void umNomeLegal(void *p, char tipo)
 // {
@@ -18,25 +19,34 @@
 
 //___________________________________________________________________________
 
-typedef void (*Formatar)(void *);
+// typedef void (*Formatar)(void *);
 
-void exibirComoInteiro(void *p)
-{
-    printf("Processando Inteiro: %d\n", *(int *)p);
-}
+// void exibirComoInteiro(void *p)
+// {
+//     printf("Processando Inteiro: %d\n", *(int *)p);
+// }
 
-void exibirComoFloat(void *p)
-{
-    printf("Processando Float: %.2f\n", *(float *)p);
-}
+// void exibirComoFloat(void *p)
+// {
+//     printf("Processando Float: %.2f\n", *(float *)p);
+// }
 
-void processadorDeDados(void *endereco, Formatar acao)
+// void processadorDeDados(void *endereco, Formatar acao)
+// {
+//     if (endereco != NULL && acao != NULL)
+//     {
+//         acao(endereco);
+//     }
+// }
+
+//____________________________________________________________________________
+
+typedef struct
 {
-    if (endereco != NULL && acao != NULL)
-    {
-        acao(endereco);
-    }
-}
+    int id;
+    char nome[50];
+    float salario;
+} RegistroPessoa;
 
 int main()
 {
@@ -49,11 +59,30 @@ int main()
 
     //________________________________________________________________________
 
-    int idade = 25;
-    float salario = 3500.50;
+    // int idade = 25;
+    // float salario = 3500.50;
 
-    processadorDeDados(&idade, exibirComoInteiro);
-    processadorDeDados(&salario, exibirComoFloat);
+    // processadorDeDados(&idade, exibirComoInteiro);
+    // processadorDeDados(&salario, exibirComoFloat);
 
+    //________________________________________________________________________
+
+    RegistroPessoa usuario;
+
+    printf("=== CADASTRO DE USUARIO ===\n");
+
+    printf("Informe o ID: ");
+    scanf("%d", &usuario.id);
+
+    getchar();
+    printf("Informe o nome: ");
+    fgets(usuario.nome, 50, stdin);
+    usuario.nome[strcspn(usuario.nome, "\n")] = 0;
+
+    printf("Informe o salario: ");
+    scanf("%f", &usuario.salario);
+
+    printf("\n--- Dados Cadastrados ---\n");
+    printf("ID: %d\nNome: %s\nSalario: R$ %.2f", usuario.id, usuario.nome, usuario.salario);
     return 0;
 }
