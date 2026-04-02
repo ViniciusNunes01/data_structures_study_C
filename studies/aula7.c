ddb5a05efcaf4ca21c63922e065f54141fda05b7
ViniciusNunes01 <sanjip415@gmail.com>
2026-04-02 09:52:51 -0300

    aula 7

--- studies/aula7.c
+++ studies/aula7.c
@@ -4,22 +4,38 @@
 int main()
 {
 
+    // FILE *arquivo;
+    // arquivo = fopen("teste.txt", "a");
+
+    // if (arquivo == NULL)
+    // {
+    //     printf("Erro ao abrir!\n");
+    //     return 1;
+    // }
+
+    // char mensagem[100];
+    // printf("Digite o seu texto: ");
+    // scanf("%[^\n]", mensagem);
+
+    // fprintf(arquivo, "%s\n", mensagem);
+
+    // fclose(arquivo);
+
     FILE *arquivo;
-    arquivo = fopen("teste.txt", "a");
+    arquivo = fopen("teste.txt", "r");
 
     if (arquivo == NULL)
     {
-        printf("Erro ao abrir!\n");
         return 1;
     }
 
-    char mensagem[100];
-    printf("Digite o seu texto: ");
-    scanf("%[^\n]", mensagem);
+    char buffer[100];
 
-    fprintf(arquivo, "%s\n", mensagem);
+    while (fscanf(arquivo, "%s", buffer) != EOF)
+    {
+        printf("Palavra lida: %s\n", buffer);
+    }
 
     fclose(arquivo);
-
     return 0;
 }
